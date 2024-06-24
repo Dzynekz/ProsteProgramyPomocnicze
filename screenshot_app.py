9c9f27746823aaaad2d16b0686ed9e49494a7b06
Dzynekz <marcinzwonik01@gmail.com>
2024-06-24 14:22:53 +0200

    Initial commit

---
+++ screenshot_app.py
@@ -0,0 +1,83 @@
+import tkinter as tk
+import pyautogui
+import keyboard
+import io
+import win32clipboard
+import time
+import threading
+
+
+
+
+class ScreenCaptureApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.withdraw()  # Ukryj główne okno
+        self.capture_area = None
+        self.rect = None
+        self.start_x = self.start_y = 0
+
+    def capture_screen(self):
+        self.capture_area = tk.Toplevel(self.root)
+        self.capture_area.attributes('-fullscreen', True)
+        self.capture_area.attributes('-alpha', 0.3)
+
+        # Użycie Canvas do rysowania prostokąta
+        self.canvas = tk.Canvas(self.capture_area, cursor="cross")
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+        self.canvas.bind('<Button-1>', self.on_button_press)
+        self.canvas.bind('<B1-Motion>', self.on_move_press)
+        self.canvas.bind('<ButtonRelease-1>', self.on_button_release)
+
+    def on_button_press(self, event):
+        self.start_x = event.x
+        self.start_y = event.y
+        self.rect = self.canvas.create_rectangle(self.start_x, self.start_y, self.start_x, self.start_y, outline='red', width=2)
+
+    def on_move_press(self, event):
+        cur_x, cur_y = (event.x, event.y)
+        self.canvas.coords(self.rect, self.start_x, self.start_y, cur_x, cur_y)
+
+    def on_button_release(self, event):
+        end_x, end_y = (event.x, event.y)
+        self.capture_area.destroy()  # Zniszcz okno zaznaczania
+
+        x1 = min(self.start_x, end_x)
+        y1 = min(self.start_y, end_y)
+        x2 = max(self.start_x, end_x)
+        y2 = max(self.start_y, end_y)
+
+        # Przechwycenie zrzutu ekranu zaznaczonego obszaru
+        screenshot = pyautogui.screenshot(region=(x1, y1, x2-x1, y2-y1))
+
+        # Konwersja obrazu do formatu zgodnego ze schowkiem
+        output = io.BytesIO()
+        screenshot.save(output, format='BMP')
+        data = output.getvalue()[14:]
+        output.close()
+
+        # Skopiowanie obrazu do schowka
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
+        win32clipboard.CloseClipboard()
+
+        print("Screenshot saved to clipboard.")
+        self.root.withdraw()  # Ukryj główne okno ponownie
+
+        
+def on_hotkey():
+    print("hotkey activated")
+    root.after(0, app.capture_screen)
+
+root = tk.Tk()
+app = ScreenCaptureApp(root) 
+
+def main():
+    keyboard.add_hotkey('ctrl+shift+s', on_hotkey, suppress=True, trigger_on_release=True)
+    while True:
+        time.sleep(1)  # Zapobiega zużyciu 100% CPU
+
+# Run the main loop in a separate thread
+threading.Thread(target=main, daemon=True).start()
+root.mainloop()
